772db58c053c0e5e7c0ae10fb933f2f374135bec
Dante Nilsson <dante.a.nilsson@gmail.com>
2025-01-21 18:16:55 +0100

    Fix: iostream using std

--- examples/3_iostream/main.cpp
+++ examples/3_iostream/main.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
-
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
 int main() {
   // Printing to the terminal
   // Using endl to flush(send the content to the terminal)
-  std::cout << "Hello world" << std::endl;
+  cout << "Hello world" << endl;
 
-  std::cout << "Write some input with spaces" << std::endl;
+  cout << "Write some input with spaces" << endl;
 
   // std::string is default exported by io stream
-  std::string input_string;
+  string input_string;
 
   // Reads from the terminal
-  std::cin >> input_string;
+  cin >> input_string;
 
   // strings can also be sent as output
-  std::cout << "The first part untill whitespace was " << input_string
-            << std::endl;
+  cout << "The first part untill whitespace was " << input_string << endl;
 
   // std::getline takes a stream and a buffer and overwrites the buffer with
   // everything in the buffer.
-  std::getline(std::cin, input_string);
+  getline(cin, input_string);
 
   // you can print like before
-  std::cout << "The rest" << input_string << std::endl;
+  cout << "The rest" << input_string << endl;
 
-  std::cout << "give a number to add 1 to instead" << std::endl;
+  cout << "give a number to add 1 to instead" << endl;
 
   // you can have other types input
   // std::string is default exported by iostream
   int input_number;
 
   // Reads a number from the terminal
-  std::cin >> input_number;
+  cin >> input_number;
 
   // strings can also be sent as output
-  std::cout << "The number inputed is: " << ++input_number << std::endl;
+  cout << "The number inputed is: " << ++input_number << endl;
 }
