a2cbd0a84b61c4e68fd967157cab3b348a83e1ca
Ludvig Källqvist Nygren <ludvigkallqvistnygren@gmail.com>
2025-01-27 15:38:47 +0100

    feat: added a justfile, watching and running of all files for testing

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,7 +5,11 @@ if [ -z "$CONFIG" ]; then
 fi
 
 for file in ./bin/$CONFIG/*; do
+<<<<<<< HEAD
   name=$(basename $file)
   echo ">>>> running $name >>>>"
  $file && echo "<<<< $name done <<<<"
+=======
+ $file
+>>>>>>> 225fe56 (feat: added a justfile, watching and running of all files for testing)
 done
