18ec731e67326482d8c04f98dba9e25fdeeadf1f
Cindy Nilsson <dante.a.nilsson@gmail.com>
2025-02-10 12:28:17 +0100

    Fix: version naming

--- scripts/install-mdbook.sh
+++ scripts/install-mdbook.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
+MDBOOK_VERSION="0.4.44"
+ADMONISH_VERSION="1.18.0"
+KATEX_VERSION="0.9.2"
+
 set -euo pipefail
 cd "$(dirname "$0")"/..
 function eprintln() {
   >&2 echo "$1"
 }
 
-VERSION="0.4.44"
 
-eprintln "Checking if mdbook $VERSION is installed"
-if [[ "$(mdbook --version)" != "mdbook v$VERSION" ]]; then
-  eprintln "Installing mdbook $VERSION"
-  cargo install mdbook --version "$VERSION" --force
+eprintln "Checking if mdbook $MDBOOK_VERSION is installed"
+if [[ "$(mdbook --version)" != "mdbook v$MDBOOK_VERSION" ]]; then
+  eprintln "Installing mdbook $MDBOOK_VERSION"
+  cargo install mdbook --version "$MDBOOK_VERSION" --force
 fi
-eprintln "mdbook $VERSION is installed"
+eprintln "mdbook $MDBOOK_VERSION is installed"
 
 # Build book, using styles present in the repository.
 
-VERSION="1.18.0"
 
-if [[ "$(mdbook-admonish --version)" != "mdbook-admonish $VERSION" ]]; then
-  eprintln "Installing mdbook-admonish $VERSION"
+if [[ "$(mdbook-admonish --version)" != "mdbook-admonish $ADMONISH_VERSION" ]]; then
+  eprintln "Installing mdbook-admonish $ADMONISH_VERSION"
   cargo install mdbook-admonish --force
 fi
 
-VERSION="0.9.2"
+eprintln "mdbook $ADMONISH_VERSION is installed"
 
-if [[ "$(mdbook-katex --version)" != "mdbook-katex $VERSION" ]]; then
-  eprintln "Installing mdbook-katex $VERSION"
+if [[ "$(mdbook-katex --version)" != "mdbook-katex $KATEX_VERSION" ]]; then
+  eprintln "Installing mdbook-katex $KATEX_VERSION"
   cargo install mdbook-katex --force
 fi
 
+eprintln "mdbook $KATEX_VERSION is installed"
 eprintln "Installing mdbook-admonish (to book)"
 mdbook-admonish install .
 
