ef335a7492119e9577ea231c6ff9c849f38e15a3
Cindy Nilsson <dante.a.nilsson@gmail.com>
2025-02-10 12:49:40 +0100

    Fix: fixing merge conflict

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -5,17 +5,7 @@ if [ -z "$CONFIG" ]; then
 fi
 
 for file in ./bin/$CONFIG/*; do
-<<<<<<< HEAD
-<<<<<<< HEAD
   name=$(basename $file)
   echo ">>>> running $name >>>>"
  $file && echo "<<<< $name done <<<<"
-=======
- $file
->>>>>>> 225fe56 (feat: added a justfile, watching and running of all files for testing)
-=======
-  name=$(basename $file)
-  echo ">>>> running $name >>>>"
- $file && echo "<<<< $name done <<<<"
->>>>>>> aa22dd8 (fix: build step improvements)
 done
